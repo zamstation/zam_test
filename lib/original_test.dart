234012bf1487600c719f5e05dac16eee600a6619
amsakanna <amsakanna@gmail.com>
2021-06-18 16:35:07 +0530

    Feature: Export original test library from zam_test.dart #12
    Export original test library from zam_test.dart.
    Remove original_test.dart

--- lib/original_test.dart
+++
@@ -1,1 +0,0 @@
-export 'package:test/test.dart';
--- lib/zam_test.dart
+++ lib/zam_test.dart
@@ -1,3 +1,5 @@
+export 'package:test/test.dart';
+
 export 'src/matchers/_.index.dart';
 export 'src/test/_.index.dart';
 export 'src/test_case/_.index.dart';
